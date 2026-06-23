e42faee4cbfae6d4579533229f27e290296030a3
wongsatorn <wchaitosa@gmail.com>
2026-06-23 21:18:48 +0700

    Homework

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,6 @@
 
 let totalFileSize = 250000;
 let uploadedSize = 30000;
+let uploadProgress = (uploadedSize / totalFileSize) * 100; // = 12 (%)
+console.log(uploadProgress); // = 12
 
-// Start coding here
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discountAmount = totalPrice * 0.25;
+let finalPrice = totalPrice - discountAmount;
+console.log(discountAmount); // = 6750
